034b7f8c6d954f17a6c807b880e0593bf30a083b
JuanjoLetelier <ju.letelier@duocuc.cl>
2025-07-11 00:52:25 -0400

    se agrega boton routerlink de carrito a pago

--- ferremas-frontend/src/app/pages/carrito/carrito.ts
+++ ferremas-frontend/src/app/pages/carrito/carrito.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CarritoService, ProductoEnCarrito } from '../../services/carrito.service';
 import { CommonModule } from '@angular/common'; 
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
   templateUrl: './carrito.html',
   styleUrls: ['./carrito.css'],
-  imports: [CommonModule]})
+  imports: [CommonModule, RouterModule]})
+
 export class Carrito implements OnInit {
   carrito: ProductoEnCarrito[] = [];
   total = 0;
 
-  constructor(private carritoService: CarritoService) {}
+  constructor(private carritoService: CarritoService, private router: Router) {}
 
   ngOnInit(): void {
     this.cargarCarrito();
@@ -46,4 +49,12 @@ export class Carrito implements OnInit {
     this.carritoService.limpiarCarrito();
     this.cargarCarrito();
   }
+ 
+  irAPago() {
+    this.router.navigate(['/pago']);
+    }
+
+
+  
+
 }
